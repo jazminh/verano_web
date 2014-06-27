1897fbc6b1c84347393a09c6855e8c7c9078b89a
Jazmin <zoe.jazz@hotmail.com>
2014-06-27 16:21:03 -0500

    primer commit

---
+++ Practica1/guardar.php
@@ -0,0 +1,27 @@
+<?php
+echo "Hola..."
+
+echo "Goles Equipo 1 Local: " . $_POST['equipo1_local'];
+echo "<br/>";
+echo "Goles Equipo 2 Visitante: " . $_POST['equipo2_visitante'];
+echo "<br/>";
+
+
+echo "Goles Equipo 3 Local: " . $_POST['equipo3_local'];
+echo "<br/>";
+echo "Goles Equipo 4 Visitante: " . $_POST['equipo4_visitante'];
+echo "<br/>";
+
+echo "Equipo Elegido 1 Local: " . $_POST['selectEquipo5_local'];
+echo "<br/>";
+
+echo "Goles Equipo 5 Local: " . $_POST['equipo5_local'];
+echo "<br/>";
+
+echo "Equipo Elegido 1 visitante: " . $_POST['selectEquipo5_local'];
+echo "<br/>";
+
+echo "Goles Equipo 5 visitante: " . $_POST['equipo5_local'];
+echo "<br/>";
+
+?>
